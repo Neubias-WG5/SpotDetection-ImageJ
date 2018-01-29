925c561ff43504e08d391b58e20840062d85f436
rmaree <raphael.maree@ulg.ac.be>
2018-01-29 12:21:17 +0100

    Update and rename run-spot-detection.sh to run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ Xvfb $DISPLAY -auth /dev/null &
 (
 # the '(' starts a new sub shell. In this sub shell we start the worker processes:
 
-java -Xmx6000m -cp /fiji/jars/ij-1.51s.jar ij.ImageJ -headless --console -macro IJSpotDetection.ijm "input=$1, output=$2, laprad=$3, noisetol=$4"
+java -Xmx6000m -cp /fiji/jars/ij-1.51s.jar ij.ImageJ -headless --console -macro macro.ijm "input=$1, output=$2, laprad=$3, noisetol=$4"
 wait # waits until all 'program' processes are finished
 # this wait sees only the 'program' processes, not the Xvfb process
 )
